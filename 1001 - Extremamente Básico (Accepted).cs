e468830a498cfdd54e2599182d31e3c70099ddab
Jeferson Santin <jeferson.luis.santin@gmail.com>
2023-05-04 08:50:10 -0300

    Create 1001 - Extremamente Básico (Accepted).cs

---
+++ 1001 - Extremamente Básico (Accepted).cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  static void Main(string[] args) {
+    int A, B, X;
+    A = int.Parse(Console.ReadLine());
+    B = int.Parse(Console.ReadLine());
+    X = A + B;
+    Console.WriteLine("X = " + X);
+  }
+}
