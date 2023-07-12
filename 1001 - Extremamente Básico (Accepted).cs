fafb0d47f9c941845ba14cc5ff968c03fd563a51
Jeferson Santin <jeferson.luis.santin@gmail.com>
2023-07-12 16:08:49 -0300

    Update 1001 - Extremamente Básico (Accepted).cs

--- 1001 - Extremamente Básico (Accepted).cs
+++ 1001 - Extremamente Básico (Accepted).cs
@@ -1,11 +1,12 @@
 using System;
 
-class Program {
-  static void Main(string[] args) {
-    int A, B, X;
-    A = int.Parse(Console.ReadLine());
-    B = int.Parse(Console.ReadLine());
-    X = A + B;
-    Console.WriteLine("X = " + X);
-  }
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int X = A + B;
+        Console.WriteLine("X = " + X);
+    }
 }
